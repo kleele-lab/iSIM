e8b5f50572a128ed2c43d18c78f5fe8eedd6bf94
Willi Stepp <willi.stepp@epfl.ch>
2021-04-22 18:46:19 +0200

    Added Java Ghost object that can be transfered directly from Java

--- MMevents.py
+++ MMevents.py
@@ -1,47 +1,63 @@
+#%%
 from pycromanager import Bridge, core
 import pycromanager
 import threading
+import re
 import zmq
 import time
 from datetime import datetime
+import json
+
+topics = ["ExposureChangedEvent", "AcquisitionSequenceStartedEvent", "AcquisitionStartedEvent",
+          "DataProviderHasNewImageEvent", "test"]
+#%%
+bridge = Bridge()
+studio = bridge.get_studio()
+acquisition = studio.get_acquisition_manager()
 
 
-# context = zmq.Context()
-# sockedt = context.socket(zmq.REQ)
-# socket.connect("tcp://localhost:5556")  #5556")
-
-
-# while True:
-#     try:
-#         t1 = time.process_time()
-#         print('Waiting for new event')
-#         socket.send_string("Hello")
-#         #  Get the reply.
-#         message = socket.recv()
-#         print(time.process_time() - t1)
-#     except KeyboardInterrupt:
-#             break
-
-
-
+#%%
 #PUB/SUB
 context = zmq.Context()
 socket = context.socket(zmq.SUB)
-socket.connect("tcp://localhost:5560")  #5556")
-topicfilter = "topic1"
-socket.setsockopt_string(zmq.SUBSCRIBE, topicfilter)
+
+socket.connect("tcp://localhost:5556")  #5556")
+for topic in topics:
+    socket.setsockopt_string(zmq.SUBSCRIBE, topic)
+socket.setsockopt(zmq.RCVTIMEO, 1000)
 
 while True:
     try:
         t1 = time.process_time()
-        print('Waiting for new event')
         #  Get the reply.
-        message = socket.recv()
+        reply = str(socket.recv())
+        print(reply)
+
+        topic = re.split(' ', reply)[0][2:]
+        print(topic)
+        print(re.split(' ', reply)[1][0:-1])
+        message = json.loads(re.split(' ', reply)[1][0:-1])
         print(message)
-        print(datetime.utcnow())
+        evt = bridge._class_factory.create(message)(socket=bridge._master_socket, serialized_object=message, bridge=bridge)
+        print(evt.get_new_exposure_time())
+
+
+        if topic == "AcquisitionSequenceStartedEvent":
+            settings = acquisition.get_acquisition_settings()
+        elif topic == "AcquisitionStartedEvent":
+            t1 = time.perf_counter()
+            eng = acquisition.get_acquisition_engine()
+            datastore = eng.get_acquisition_datastore()
+            print(time.perf_counter() - t1)
+
+
+
     except KeyboardInterrupt:
+        print('Closing socket')
         socket.close()
         break
+    except zmq.error.Again:
+        pass
 
 
 
