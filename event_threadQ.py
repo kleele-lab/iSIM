0a58ec7834610dce8da89b27e3b59ea9267614af
Willi Stepp <willi.stepp@epfl.ch>
2022-01-26 18:37:16 +0100

    reduce prints

--- event_threadQ.py
+++ event_threadQ.py
@@ -7,13 +7,13 @@ import json
 from PyQt5.QtCore import QObject, pyqtSignal, QThread, pyqtSlot
 import time
 
-from data_structures import PyImage, MMSettings
+from isimgui.data_structures import PyImage, MMSettings
 
-SOCKET = '5556'
+SOCKET = "5556"
+
 
 class EventThread(QObject):
-    """ Thread that receives events from Micro-Manager and relays them to the main program"""
-
+    """Thread that receives events from Micro-Manager and relays them to the main program"""
 
     def __init__(self):
         super().__init__()
@@ -24,8 +24,9 @@ class EventThread(QObject):
         self.event_sockets = []
         self.num_sockets = 5
         for socket in range(self.num_sockets):
-            socket_provider = self.bridge.construct_java_object('org.micromanager.Studio',
-                                                                new_socket=True)
+            socket_provider = self.bridge.construct_java_object(
+                "org.micromanager.Studio", new_socket=True
+            )
             self.event_sockets.append(socket_provider._socket)
 
         # PUB/SUB
@@ -48,12 +49,13 @@ class EventThread(QObject):
 
     def stop(self):
         self.listener.stop()
-        print('Closing socket')
+        print("Closing socket")
         self.thread.exit()
         self.socket.close()
         for socket in self.event_sockets:
             socket.close()
         self.context.term()
+
 
 class EventListener(QObject):
     xy_stage_position_changed_event = pyqtSignal(tuple)
@@ -79,6 +81,7 @@ class EventListener(QObject):
         self.blockImages = False
 
     pyqtSlot()
+
     def start(self):
         instance = 0
         while not self.loop_stop:
@@ -87,78 +90,84 @@ class EventListener(QObject):
                 #  Get the reply.
                 reply = str(self.socket.recv())
                 # topic = re.split(' ', reply)[0][2:]
-                message = json.loads(re.split(' ', reply)[1][0:-1])
+                message = json.loads(re.split(" ", reply)[1][0:-1])
                 socket_num = instance % len(self.event_sockets)
                 pre_evt = self.bridge._class_factory.create(message)
 
                 evt = pre_evt(
                     socket=self.event_sockets[socket_num],
-                                                            serialized_object=message,
-                                                            bridge=self.bridge)
+                    serialized_object=message,
+                    bridge=self.bridge,
+                )
 
-                eventString = message['class'].split(r'.')[-1]
-                print(eventString, ' ', time.perf_counter())
-                if 'ExposureChangedEvent' in eventString:
+                eventString = message["class"].split(r".")[-1]
+                print(eventString, " ", time.perf_counter())
+                if "ExposureChangedEvent" in eventString:
                     print(evt.get_new_exposure_time())
-                elif 'DefaultAcquisitionStartedEvent' in eventString:
+                elif "DefaultAcquisitionStartedEvent" in eventString:
                     if time.perf_counter() - self.last_acq_started > 0.2:
                         self.acquisition_started_event.emit(evt)
-                        print('ACQ started Event emitted ', time.perf_counter())
                     else:
-                        print('SKIPPED')
+                        print("SKIPPED")
                     self.last_acq_started = time.perf_counter()
-                elif 'DefaultAcquisitionEndedEvent' in eventString:
+                elif "DefaultAcquisitionEndedEvent" in eventString:
                     self.acquisition_ended_event.emit(evt)
-                    print('Acquisition Ended')
-                elif 'DefaultStagePositionChangedEvent' in eventString:
-                    if self.blockZ > 0 or time.perf_counter() - self.last_stage_position < 0.05:
+                elif "DefaultStagePositionChangedEvent" in eventString:
+                    if (
+                        self.blockZ > 0
+                        or time.perf_counter() - self.last_stage_position < 0.05
+                    ):
                         print("BLOCKED ", self.blockZ)
                     else:
-                        self.stage_position_changed_event.emit(evt.get_pos()*100)
+                        self.stage_position_changed_event.emit(evt.get_pos() * 100)
                     self.last_stage_position = time.perf_counter()
                     self.blockZ = False
-                elif 'XYStagePositionChangedEvent' in eventString:
-                    self.xy_stage_position_changed_event.emit((evt.get_x_pos(), evt.get_y_pos()))
-                elif 'DefaultNewImageEvent' in eventString:
+                elif "XYStagePositionChangedEvent" in eventString:
+                    self.xy_stage_position_changed_event.emit(
+                        (evt.get_x_pos(), evt.get_y_pos())
+                    )
+                elif "DefaultNewImageEvent" in eventString:
                     if self.blockImages:
                         return
                     image = evt.get_image()
-                    py_image = PyImage(image.get_raw_pixels().reshape([image.get_width(),
-                                                                       image.get_height()]),
-                                       image.get_coords().get_t(),
-                                       image.get_coords().get_c(),
-                                       image.get_metadata().get_elapsed_time_ms())
-                                    #  0) # no elapsed time
+                    py_image = PyImage(
+                        image.get_raw_pixels().reshape(
+                            [image.get_width(), image.get_height()]
+                        ),
+                        image.get_coords().get_t(),
+                        image.get_coords().get_c(),
+                        image.get_metadata().get_elapsed_time_ms(),
+                    )
+                    #  0) # no elapsed time
                     self.new_image_event.emit(py_image)
-                elif 'CustomSettingsEvent' in eventString:
-                    self.settings_event.emit(evt.get_device(), evt.get_property(), evt.get_value())
-                elif 'CustomMDAEvent' in eventString:
+                elif "CustomSettingsEvent" in eventString:
+                    self.settings_event.emit(
+                        evt.get_device(), evt.get_property(), evt.get_value()
+                    )
+                elif "CustomMDAEvent" in eventString:
                     if time.perf_counter() - self.last_custom_mda > 0.2:
                         settings = evt.get_settings()
                         # print(dir(settings))
                         settings = MMSettings(java_settings=settings)
                         self.mda_settings_event.emit(settings)
                     else:
-                        print('SKIPPED')
+                        print("SKIPPED")
                     self.last_custom_mda = time.perf_counter()
                 elif "DefaultLiveModeEvent" in eventString:
                     self.blockImages = evt.get_is_on()
                     self.live_mode_event.emit(self.blockImages)
                     print("Blocking images in live: ", self.blockImages)
 
-
-
                 else:
-                    print('This event is not known yet')
+                    print("This event is not known yet")
             except zmq.error.Again:
                 pass
         # Thread was stopped, let's also close the socket then
 
-
     pyqtSlot()
+
     def stop(self):
         self.loop_stop = True
-
 
 
 def main():
@@ -169,8 +178,9 @@ def main():
             time.sleep(0.01)
         except KeyboardInterrupt:
             thread.stop()
-            print('Stopping')
+            print("Stopping")
             break
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
