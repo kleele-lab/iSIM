c19c4479cb48ec390d81f81c795d78ec5ddb6449
Willi Stepp <willi.stepp@epfl.ch>
2022-08-03 14:02:24 +0200

    outsource event_thread

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from data_structures import MMSettings
 from gui.GUIWidgets import SettingsView
 import sys
 from PyQt5 import QtWidgets
-from event_threadQ import EventThread
+from pymm_eventserver.event_thread import EventThread
 from gui.MainGUI import MainGUI
 from hardware.nidaq import NIDAQ
 
@@ -11,21 +11,17 @@ from hardware.nidaq import NIDAQ
 def main():
     app = QtWidgets.QApplication(sys.argv)
 
-
     event_thread = EventThread()
     event_listener = event_thread.listener
 
     mm_interface = MicroManagerControl(event_listener)
-
 
     ni = NIDAQ(event_listener, mm_interface)
     settings_view = SettingsView(event_listener)
     miniapp = MainGUI(event_thread=event_listener)
     miniapp.show()
 
-
     sys.exit(app.exec_())
-
 
 
 if __name__ == "__main__":
