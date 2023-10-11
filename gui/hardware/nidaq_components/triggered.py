1b42b2361625715e9c709af9a1f85d065ed0139f
Willi Stepp <willi.stepp@epfl.ch>
2023-10-11 11:34:13 +0200

    first test of triggering from command line

---
+++ gui/hardware/nidaq_components/triggered.py
@@ -0,0 +1,51 @@
+import nidaqmx
+import nidaqmx.stream_writers
+import numpy as np
+
+def sine():
+    # Define the parameters
+    frequency = 3.1  # Frequency of the sine wave in Hz
+    duration = 1  # Duration in seconds
+    sample_rate = 100  # Sampling rate in samples per second
+
+    # Calculate the number of samples
+    num_samples = int(duration * sample_rate)
+
+    # Create a time vector
+    t = np.linspace(0, duration, num_samples, endpoint=False)
+
+    # Generate a sine wave
+    sine_wave = np.sin(2 * np.pi * frequency * t) * 5 + 5
+    sine_wave = np.append(sine_wave, np.zeros(1))
+    return sine_wave
+
+def change_led(LED: bool, task: nidaqmx.Task):
+    LED = not LED
+    data = np.zeros(101) if not LED else sine()
+    print(len(data))
+    task.write(data)
+
+def print_done():
+    print("DONE with this frame!")
+
+try:
+    LED = True
+    task = nidaqmx.Task()
+    task.ao_channels.add_ao_voltage_chan('Dev1/ao6')
+    task.timing.cfg_samp_clk_timing(rate=100, sample_mode=nidaqmx.constants.AcquisitionType.CONTINUOUS)
+    task.register_done_event(print_done)
+    task.start()
+    while True:
+        # Use the input function to wait for Enter key press
+        user_input = input("Press Enter to continue...")
+        LED = not LED
+        # Check if the user_input is an empty string (Enter key was pressed)
+        if user_input == "":
+            change_led(LED, task)
+            continue  # Continue the loop if Enter key is pressed
+        else:
+            print("You didn't press Enter. Please press Enter to continue.")
+except KeyboardInterrupt:
+    task.write(0)
+    task.close()
+    print("\nLoop interrupted by user.")
