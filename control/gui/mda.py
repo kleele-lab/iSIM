7633d91745aad8daf4973387b30691c5e9f4f37e
Willi Stepp <willi.stepp@epfl.ch>
2023-10-17 15:51:29 +0200

    starting to put central datastructure

--- control/gui/mda.py
+++ control/gui/mda.py
@@ -1,14 +1,26 @@
 from pymmcore_widgets.mda._core_mda import MDAWidget
-from qtpy.QtWidgets import QApplication
+from qtpy.QtWidgets import QApplication, QPushButton
 from pymmcore_plus import CMMCorePlus
 
 mmc = CMMCorePlus()
 mmc.loadSystemConfiguration()
 
+import useq
+def print_seq(self, seq: useq.MDASequence):
+    print(seq.yaml())
 
 
-app = QApplication([])
-frame = MDAWidget(mmcore=mmc)
-frame.setWindowTitle("MyMDA")
-frame.show()
-app.exec_()
+
+
+if __name__ == "__main__":
+
+    app = QApplication([])
+    frame = MDAWidget(mmcore=mmc)
+    frame.setWindowTitle("MyMDA")
+    frame.show()
+
+    button = QPushButton("Test")
+    button.pressed.connect(lambda: print_seq(None, frame.value()))
+    button.show()
+
+    app.exec_()
